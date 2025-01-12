891fc9f1e8187ed554bb15cf2ff13bed9af55c1b
Ofir Levi <ofirl@natali.co.il>
2025-01-12 17:03:50 +0200

    This is the second commit = adding new app.js file

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (request, reponse) => {
+	respose.end('Hello World!');
+}
+
+const server = http.createServer(requestHandler);
+server.listen(port, (err) => {
+	if (err) {
+		return console.loc('Error:', err);
+	}
+	console.log('Server listening on port ${port}');
+});
