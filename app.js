40df57f554023fd939d6f8219c15bb9ef2d17175
Ofir Levi <ofirl@natali.co.il>
2025-01-13 14:20:12 +0200

    This is the seventh commit - fixing misspleing on app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const port = 3000;
 
-const requestHandler = (request, reponse) => {
-	respose.end('Hello World!');
+const requestHandler = (request, response) => {
+	response.end('Hello World!');
 }
 
 const server = http.createServer(requestHandler);
